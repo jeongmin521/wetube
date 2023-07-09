c9ec6182bba2e19f21cb0e10e4d183a28547c20e
jeongmin <jeongmin0521@naver.com>
2023-07-09 15:13:50 +0900

    getEdit

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -8,17 +8,20 @@ export const home = async(req, res) => {
 export const watch = async (req, res) => {
     const { id } = req.params;
     const video = await Video.findById(id);
-    if(video){
-        return res.render("watch", {pageTitle: video.title, video });
+    if(!video){
+        return res.render("404", { pageTitle: "Video not found." });
     }
-    return res.render("404", { pageTitle: "Video not found." });
+    return res.render("watch", {pageTitle: video.title, video });
 };
 
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
     const { id } = req.params;
-    const video = videos[id-1];
-    return res.render("edit", { pageTitle: `Editing` });
-}; //form을 화면에 보여줌
+    const video = await Video.findById(id);
+    if(!video){
+        return res.render("404", { pageTitle: "Video not found." });
+    }
+    return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
+};
 
 export const postEdit = (req, res) => {
     const { id } = req.params; //req.param에서 id를 얻고
