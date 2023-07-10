f957c185c81c6bdeef314a160e467eb9d5384b65
jeongmin <jeongmin0521@naver.com>
2023-07-10 12:04:01 +0900

    static

--- src/models/Video.js
+++ src/models/Video.js
@@ -1,9 +1,5 @@
 import mongoose from "mongoose";
 
-export const formatHashtags = (hashtags) => 
-    hashtags.split(",").map((word) => (word.startsWith("#") ? word : `#${word}`))
-
-
 const videoSchema = new mongoose.Schema({
     title: { type: String, required: true, trim: true, maxLength: 80 },
     description: { type: String, required: true, trim: true, minLength: 20 },
@@ -15,5 +11,12 @@ const videoSchema = new mongoose.Schema({
     },
 });
 
-const Video = mongoose.model("Video", videoSchema );
+videoSchema.static('formatHashtags',function(hashtags) {
+    return hashtags
+        .split(",")
+        .map((word) => (word.startsWith("#") ? word : `#${word}`));
+});
+
+const Video = mongoose.model("Video", videoSchema);
+
 export default Video;
