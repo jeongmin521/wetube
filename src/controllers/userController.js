ede68b76e78a7f43fdee10aec2f9bbdfc2040140
jeongmin <jeongmin0521@naver.com>
2023-07-11 13:29:04 +0900

    password hashing

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,19 @@
-export const getJoin = (req, res) => 
-    res.render("join", { pageTitle: "Join" } );
-export const postJoin = (req, res) => {
-    console.log(req.body);
-    res.end();
+import User from "../models/User";
+
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" } );
+
+export const postJoin = async(req, res) => {
+    const { name, username, email, password, location } = req.body;
+    await User.create({
+        name,
+        username,
+        email,
+        password,
+        location,
+    });
+    return res.redirect("/login"); //계정을 생성하면 로그인 페이지로 이동
 };
+
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
 export const login = (req, res) => res.send("Login");
