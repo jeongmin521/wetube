11d7362bc1715347f259a346d8c063147c1ff6de
jeongmin <jeongmin0521@naver.com>
2023-08-03 00:32:48 +0900

    깃허브 로그인

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -110,11 +110,20 @@ export const finishGithubLogin = async(req, res) => {
             },
         })
     ).json();
-    const email = emailData.find(
+    const emailObj = emailData.find(
         (email) => email.primary === true && email.verified === true);
-        if(!email){
+        if(!emailObj){
             return res.redirect("/login");
+        }
+        const existingUser = await User.findOne({ email: emailObj.email });
+        if(existingUser){
+            req.session.loggedIn = true;
+            req.session.user = existingUser;
+            return res.redirect("/");
         } else {
+
+        }
+    } else {
         return res.redirect("/login");
     }
 };
