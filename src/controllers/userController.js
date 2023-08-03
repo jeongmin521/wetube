e6658eac63ad644ffaa9aeea9b02e691ef373952
jeongmin <jeongmin0521@naver.com>
2023-08-03 17:24:08 +0900

    로그아웃

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -135,7 +135,10 @@ export const finishGithubLogin = async(req, res) => {
     }
 };
 
+export const logout = (req, res) => {
+    req.session.destroy();
+    return res.redirect("/");
+};
+
 export const edit = (req, res) => res.send("Edit User");
-export const remove = (req, res) => res.send("Remove User");
-export const logout = (req, res) => res.send("Logout");
 export const see = (req, res) => res.send("See User");
