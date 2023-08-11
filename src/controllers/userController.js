961579f41bc13c7b8274dda3c8c74257fc89d92c
jeongmin <jeongmin0521@naver.com>
2023-08-11 15:21:43 +0900

    my profile

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -151,7 +151,6 @@ export const postEdit = async (req, res) => {
         body: { name, email, username, location },
         file,
     } = req;
-    console.log(file);
     const updateUser = await User.findByIdAndUpdate( 
         _id, 
         {
@@ -201,4 +200,14 @@ export const postChangePassword = async (req, res) => {
     return res.redirect("/users/logout");
 };
 
-export const see = (req, res) => res.send("See User");
+export const see = async (req, res) => {
+    const{ id } = req.params;
+    const user = await User.findById(id);
+    if(!user){
+        return res.status(404).render("404",{ pageTitle: "User not found " });
+    }
+    return res.render("users/profile", {
+        pageTitle: user.name, 
+        user,
+     });
+};
