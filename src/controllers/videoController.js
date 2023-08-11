ca2e791429a6512f033798b22ec62b0ceb5fcaf5
jeongmin <jeongmin0521@naver.com>
2023-08-11 16:22:48 +0900

    populate

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -48,16 +48,19 @@ export const postUpload = async (req, res ) => {
     const{
         user: {_id },
     } = req.session;
-    const file = req.file;
+    const {path: fileUrl} = req.file;
     const { title, description, hashtags } = req.body;
     try{
-        await Video.create({
+        const newVideo = await Video.create({
             title,
             description,
-            fileUrl: file.path,
+            fileUrl,
             owner: _id,
             hashtags: Video.formatHashtags(hashtags),
         });
+        const user = await User.findById(_id);
+        user.videos.push(newVideo._id);
+        user.save();
         return res.redirect("/");
     }catch (error){
         return res.status(400).render("upload", {
