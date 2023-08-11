da68c4e7b86755a746e9e0534949fdde19c33580
jeongmin <jeongmin0521@naver.com>
2023-08-11 16:09:49 +0900

    user profile

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,5 @@
 import Video from "../models/Video";
+import User from "../models/User";
 
 export const home = async(req, res) => {
     const videos = await Video.find({}).sort({createdAt: "asc" });
@@ -7,7 +8,7 @@ export const home = async(req, res) => {
 
 export const watch = async (req, res) => {
     const { id } = req.params;
-    const video = await Video.findById(id);
+    const video = await Video.findById(id).populate("owner");
     if(!video){
         return res.render("404", { pageTitle: "Video not found." });
     }
@@ -44,6 +45,9 @@ export const getUpload = (req, res) => {
 };
 
 export const postUpload = async (req, res ) => {
+    const{
+        user: {_id },
+    } = req.session;
     const file = req.file;
     const { title, description, hashtags } = req.body;
     try{
@@ -51,6 +55,7 @@ export const postUpload = async (req, res ) => {
             title,
             description,
             fileUrl: file.path,
+            owner: _id,
             hashtags: Video.formatHashtags(hashtags),
         });
         return res.redirect("/");
