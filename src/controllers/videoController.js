4c2667a6d05a94f43d7f700e464682dd189c0f0c
jeongmin <jeongmin0521@naver.com>
2023-08-13 00:48:45 +0900

    style

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,7 +2,7 @@ import Video from "../models/Video";
 import User from "../models/User";
 
 export const home = async(req, res) => {
-    const videos = await Video.find({}).sort({createdAt: "asc" });
+    const videos = await Video.find({}).sort({createdAt: "desc" }).populate("owner");
     return res.render("home", { pageTitle:"Home", videos });
 };
 
