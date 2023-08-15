e65a8ad7b0ea351142f42ec309d93472af621bf1
jeongmin <jeongmin0521@naver.com>
2023-08-16 00:44:52 +0900

    style

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -203,7 +203,13 @@ export const postChangePassword = async (req, res) => {
 
 export const see = async (req, res) => {
     const{ id } = req.params;
-    const user = await User.findById(id).populate("videos");
+    const user = await User.findById(id).populate({
+        path: "videos",
+        populate: {
+            path: "owner",
+            model: "user",
+        },
+    });
     if(!user){
         return res.status(404).render("404",{ pageTitle: "User not found" });
     }
@@ -211,6 +217,5 @@ export const see = async (req, res) => {
     return res.render("users/profile", {
         pageTitle: user.name, 
         user,
-        videos,
      });
 };
